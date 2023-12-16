e1fd685b2be8d0f843aee998b1b1eb229e9adbef
Subodh Ghimire <ghimiresubodh59@gmail.com>
2023-12-16 12:57:07 +0545

    Basics of Python

---
+++ Basics.py
@@ -0,0 +1,39 @@
+#Basics Day 1
+
+print(2+3) #"+" --> addition
+#ans-->5 
+print(11/2) #"/" --> division
+#ans-->5.5
+print(2*5) #"*" --> multiply
+#ans-->10 
+print(2**5) #"**" --> power
+#ans-->32 
+print(21//5) #"//" --> quotient
+#ans-->4
+print(30 % 5) #"%" --> remainder
+#ans-->0
+
+
+#String
+print("Hello")
+print('Hello')
+print('"Hello he\'s a good person"')
+
+
+#New Lines
+print("One \n Two")
+print("""He is a good 
+      boy 
+      father son""")
+
+
+#String Concatenate
+print('Hello ' + "Ma'am")
+print("2"+'2')
+print("World " * 2)
+
+
+#Variables
+a="Hello"
+b="Ma'am"
+print(a+" "+b)
