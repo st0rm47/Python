7b572bf039981cda9053240103253b99cb486af6
Subodh Ghimire <ghimiresubodh59@gmail.com>
2023-12-18 13:58:21 +0545

    Functions

---
+++ Functions.py
@@ -0,0 +1,15 @@
+#--->Functions
+#def refers to defining a function
+def pretty():
+    print("You are pretty")
+    
+pretty()
+
+#Function with arguments
+def print_word(x):
+    print("My name is " + x)
+print_word("World")
+
+
+
+AAYEU?
