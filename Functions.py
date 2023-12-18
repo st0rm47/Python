0089ca5975811f19591e17d07472b864b422f31f
Subodh Ghimire <ghimiresubodh59@gmail.com>
2023-12-18 15:48:57 +0545

    Functions

--- Functions.py
+++ Functions.py
@@ -10,6 +10,41 @@ def print_word(x):
     print("My name is " + x)
 print_word("World")
 
+def operations(x,y):
+    print(x+y)
+    print(x*y)
+    print(x/y)
+operations(4,2)
+
+#EX:Feet into inches
+feet=int(input("Enter feet: "))
+def convert(x):
+    print(str(feet)+ " feet is " + str(x*12) + " inches")
+convert(feet)
+    
+#Returning from a Function
+def maxi(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+    print("Any code written after return is not printed")
+print(maxi(5,8))
+print(maxi(7,9))
 
 
-AAYEU?
+#Example: Write a function that takes a string and a letter as its arguments 
+#And returns the count of the letter in the string.
+x=input("Enter a string: ")
+y=input("Enter a letter: ")
+
+def letter(x,y):
+    c=0
+    for a in x:
+        if a==y:
+            c+=1
+    return c
+
+print(letter(x,y))
+
+
