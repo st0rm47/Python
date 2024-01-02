67af46cd780e904d942608c6ed9dc72d8d53682e
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-01-02 16:46:44 +0545

    Assertion

--- Assert.py
+++ Assert.py
@@ -3,8 +3,14 @@ print("Hello")
 assert 2+2==4
 print ("True")
 
-temp = -10
-assert (temp >= 0), "Colder than absolute zero!"
-print(temp)
-#If the argument is true the program continues
-#ft the argument is false it raises exception
+# temp = -10
+# assert (temp >= 0), "Colder than absolute zero!"
+# print(temp)
+# #If the argument is true the program continues
+# #If the argument is false it raises exception
+
+
+try:
+    assert 2*2==5
+except AssertionError:
+    print("2*2 is 4")
