7e30508a7ff8423204e62d6aead502f2a3113f96
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-01-23 17:51:13 +0545

    REGEX

--- RegEx.py
+++ RegEx.py
@@ -9,9 +9,31 @@ if username and "." in domain:
 else:
     print("Invalid")
 
+
 #Same using RegEX
 import re
-if re.search(".+@.+\.gmail", email):
+email= input("Email:").strip()
+if re.search(r"^[^@]+@[^@]+\.edu$", email):
+    # "." refers any character except a new line
+    # "+" refers repitions of characters 1 or more time
+    # "\" refers to escape character which used to introduce string
+    # "$" refers to the end of string 
+    # "^" refers to the start of string
+    # "r" refers to treat the string as raw string
+    # "[^@]+" refers to any character except an @
     print("Valid")
 else:
     print("Invalid")
+    
+    
+#Checking that the email should be alphanumeric
+import re
+email= input("Email:").strip()
+if re.search(r"^\w+@[a-zA-Z0-9_]+\.(com|edu|gov|net|co|org)$",email):
+    # "[a-zA-Z0-9_]" tells the validation of characters
+    # "\w" refers to the validation of characters
+    # "|" refers to OR
+    print("Valid")
+else:
+    print("Invalid")
+    
