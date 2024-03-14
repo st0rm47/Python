b838ada3e182533bde5ffbae281ad7fcada1c33d
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-03-14 12:33:17 +0545

    Simple Calculator

--- Projects/Calculator.py
+++ Projects/Calculator.py
@@ -1,4 +1,5 @@
 ###--->Making a Simple Calculator
+
 def addition(a,b):
     return a+b
 def subtraction(a,b):
@@ -20,13 +21,13 @@ a=int(input("Enter 1st Number: "))
 b=int(input("Enter 2nd Number: "))
 
 #Conditions on which operand willbe performed
-if operand==1:
+if operand == 1:
     print(a,"+", b, "=", addition(a,b))
-elif operand==2:
+elif operand == 2:
     print(a,"-", b, "=", subtraction(a,b))
-elif operand==3:
+elif operand == 3:
     print(a,"*", b, "=", multiplication(a,b))
-elif operand==4:
+elif operand == 4:
     print(a,"/", b, "=", division(a,b))
 else:
     print("Invalid Operand")
