c1a36ce96ad2dcea8a90de01b3959ce9dbc6df8f
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-03-15 15:17:52 +0545

    Tic Tac Toe

--- Projects/TicTacToe.py
+++ Projects/TicTacToe.py
@@ -21,6 +21,25 @@ class Board:
         else:
             print("Position already taken!")
             return False
+        
+    def is_winner(self, type):
+        #Checking the rows
+        for i in range(0, 9, 3):
+            if self.board[i] == self.board[i + 1] == self.board[i + 2] == type:
+                return True
+        #Checking the columns
+        for i in range(3):
+            if self.board[i] == self.board[i + 3] == self.board[i + 6] == type:
+                return True
+        #Checking the diagonals
+        if self.board[0] == self.board[4] == self.board[8] == type:
+            return True
+        if self.board[2] == self.board[4] == self.board[6] == type:
+            return True
+        return False
+    
+    def check_draw(self):
+        return " " not in self.board   
 
 #Creating a Player
 class Player:
@@ -50,20 +69,31 @@ class Game:
             
                 if self.board.update_board(position, self.player.type):
                     self.board.print_board()
-                    
 
+                    if self.board.is_winner(self.player.type):
+                        print(f"{self.player.name} wins!")
+                        break
+                    elif self.board.check_draw():
+                        print("It's a draw!")
+                        break
+                    else:                   
                         #Changing the player to computer
-                self.player, self.computer = self.computer, self.player
+                        self.player, self.computer = self.computer, self.player
+                
 
             else:
                 position = random.randint(1, 9)
                 if self.board.update_board(position, self.player.type):
                     self.board.print_board()
                     
-                
-                        #Changing the player to user
-                self.player, self.computer = self.computer, self.player
-                    
-
+                    if self.board.is_winner(self.player.type):
+                        print(f"{self.player.name} wins!")
+                        break
+                    elif self.board.check_draw():
+                        print("It's a draw!")
+                        break
+                    else:                   
+                        #Changing the player to USER
+                        self.player, self.computer = self.computer, self.player
 game = Game()
 game.play()
